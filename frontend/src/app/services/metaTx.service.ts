200ea8fca2b44cfef81e727e6966949ad29ce17b
Brightiya <bightiyahen@gmail.com>
2026-02-15 15:46:17 +0100

    Fix wrong name in MetaTxService from  ForwardRequestData to  ForwardRequest

--- frontend/src/app/services/metaTx.service.ts
+++ frontend/src/app/services/metaTx.service.ts
@@ -92,7 +92,7 @@ export class MetaTxService {
     };
 
    const types = {
-  ForwardRequestData: [
+  ForwardRequest: [
     { name: "from", type: "address" },
     { name: "to", type: "address" },
     { name: "value", type: "uint256" },
